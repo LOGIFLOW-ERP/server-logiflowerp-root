a9b73e3d731b0458ad55fdfaba5a4d01da424705
usuario0888 <antonio.mejia@logiflowerp.com>
2025-10-08 10:40:48 -0500

    Enhance UseCaseSave to handle inventory updates and employee stock management for canceled orders

--- src/Context/Processes/WinOrder/Application/UseCaseSave.ts
+++ src/Context/Processes/WinOrder/Application/UseCaseSave.ts
@@ -8,7 +8,14 @@ import {
     WINOrderENTITY,
     WINOrderStockENTITY,
     validateCustom,
-    StateOrderWin
+    StateOrderWin,
+    ProductENTITY,
+    State,
+    ProducType,
+    EmployeeStockSerialENTITY,
+    StateStockSerialEmployee,
+    EmployeeENTITY,
+    ScrapingSystem
 } from 'logiflowerp-sdk'
 import { UnprocessableEntityException } from '@Config/exception'
 
@@ -38,11 +45,82 @@ export class UseCaseSave {
         const toTTL: RequestNumberTTLENTITY[] = []
         const toWinOrderStock: WINOrderStockENTITY[] = []
 
+        let dataEmployee: EmployeeENTITY[] | null = null
+
         for (const order of orders) {
             const exist = ordersMap.get(order.numero_de_peticion)
 
             if (exist) {
                 order.inventory = exist.inventory
+
+                if ([StateOrderWin.ANULADA, StateOrderWin.CANCELADA].includes(order.estado)) {
+                    const pipeline = [{
+                        $match: {
+                            itemCode: { $in: order.inventory.map(e => e.code) },
+                            state: State.ACTIVO,
+                            isDeleted: false
+                        }
+                    }]
+                    const products = await this.repository.select<ProductENTITY>(pipeline, collections.product, codeCompany)
+                    if (!dataEmployee) {
+                        const pipelineEmployee = [{
+                            $match: {
+                                'resourceSystem.system': ScrapingSystem.WIN,
+                                isDeleted: false,
+                                state: State.ACTIVO
+                            }
+                        }]
+                        dataEmployee = await this.repository.select<EmployeeENTITY>(pipelineEmployee, collections.employee, codeCompany)
+                    }
+                    const employee = dataEmployee.filter(e => e.resourceSystem.some(el => el.resource_id === order.resource_id && el.system === ScrapingSystem.WIN))
+                    if (employee.length !== 1) {
+                        throw new Error(`Hay ${employee.length} resultados para personal resource_id: ${order.resource_id} y sistema ${ScrapingSystem.WIN}`)
+                    }
+                    for (const inv of order.inventory) {
+                        const product = products.filter(e => e.itemCode === inv.code)
+                        if (product.length !== 1) {
+                            throw new Error(`Hay ${products.length} resultados para producto ${inv.code}`)
+                        }
+                        if (product[0].producType !== ProducType.SERIE) {
+                            continue
+                        }
+                        const pipeline = [{
+                            $match: {
+                                itemCode: inv.code,
+                                state: StateStockSerialEmployee.RESERVADO_CONSUMO_WIN,
+                                keySearch: { $regex: 'Nuevo$', $options: 'i' },
+                                identity: employee[0].identity,
+                                serial: inv.invsn
+                            }
+                        }]
+                        const stockSerial = await this.repository.selectOne<EmployeeStockSerialENTITY>(
+                            pipeline,
+                            collections.employeeStockSerial,
+                            codeCompany
+                        )
+                        const transaction: ITransaction<EmployeeStockSerialENTITY> = {
+                            database: codeCompany,
+                            collection: collections.employeeStockSerial,
+                            transaction: 'updateOne',
+                            filter: { _id: stockSerial._id },
+                            update: {
+                                $set: {
+                                    state: StateStockSerialEmployee.POSESION,
+                                    documentNumber: order.numero_de_peticion,
+                                    updatedate: new Date()
+                                }
+                            }
+                        }
+                        transactions.push(transaction)
+                    }
+                    toTTL.push({
+                        _id: crypto.randomUUID(),
+                        createdAt: new Date(),
+                        numero_de_peticion: order.numero_de_peticion
+                    } as RequestNumberTTLENTITY)
+                    order.inventory = []
+                }
+
                 if (exist.estado !== StateOrderWin.FINALIZADA) {
                     transactions.push({
                         database: codeCompany,
@@ -61,6 +139,7 @@ export class UseCaseSave {
                                 fecha_estado: order.fecha_estado,
                                 prioridad: order.prioridad,
                                 historial_estados: order.historial_estados,
+                                inventory: order.inventory,
                             }
                         }
                     })
